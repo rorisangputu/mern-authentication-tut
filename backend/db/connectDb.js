44914fbccfcf7053b7efb2989a7784d5bc731659
Rorisang Putu <rorisangputu@gmail.com>
2025-02-12 21:31:29 +0200

    exporting connect db

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose"
 
-const connectDb = async () => {
+export const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_CON)
+        console.log("Db Connected")
     } catch (error) {
-        
+        console.error('Db Connection Error: ', error)
+        process.exit(1);
     }
 }
