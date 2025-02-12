508fa6993375fcb56de6daae4ea2fc78a4365661
Rorisang Putu <rorisangputu@gmail.com>
2025-02-12 21:26:19 +0200

    creating index file

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import 'dotenv/config'
+
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Listening on Port 3000")
+})
