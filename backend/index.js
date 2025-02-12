5311d092562056848f3f48d960adcc168ea44245
Rorisang Putu <rorisangputu@gmail.com>
2025-02-12 21:32:48 +0200

    assigning connect db method to constant variable

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ import 'dotenv/config'
 import { connectDb } from './db/connectDb.js';
 
 
-
+const databaseConn = connectDb();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    connectDb();
+    databaseConn
     console.log("Listening on Port 3000")
 })
