245d9d56e82ad4dd77e392d844a0dd3b562cc80a
Rorisang Putu <rorisangputu@gmail.com>
2025-02-12 21:31:16 +0200

    importing connect db method

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
+import { connectDb } from './db/connectDb.js';
 
 
 
@@ -8,5 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    connectDb();
     console.log("Listening on Port 3000")
 })
