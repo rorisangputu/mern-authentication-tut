630e39a7bc061b0dfab6da17382113613a150af7
Rorisang Putu <rorisangputu@gmail.com>
2025-02-13 19:50:02 +0200

    creating signup, login, and logout routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,4 +2,14 @@ import { Router } from "express";
 
 const router = Router()
 
+router.get("/signup", (req, res) => {
+    res.send("Sign Up")
+})
+router.get("/login", (req, res) => {
+    res.send("Login")
+})
+router.get("/logout", (req, res) => {
+    res.send("Logout")
+})
+
 export default router;
