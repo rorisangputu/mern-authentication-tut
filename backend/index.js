1c7e482b02731cf0d8dfbaf4687bc5eceea77dc9
Rorisang Putu <rorisangputu@gmail.com>
2025-02-13 19:45:40 +0200

    creating auth route

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send({message: "Helloooo"})
 })
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
     databaseConn
     console.log("Listening on Port 3000")
