a960de583592bece581a740169f830cd39360eab
Rorisang Putu <rorisangputu@gmail.com>
2025-02-13 19:55:09 +0200

    creating controller and importing functions into route handler

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import bcryptjs from 'bcryptjs'
+
+export const signup = async (req, res) => {
+    
+}
+export const login = async (req, res) => {
+
+}
+export const logout = async (req, res) => {
+
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = Router()
 
-router.get("/signup", (req, res) => {
-    res.send("Sign Up")
-})
-router.get("/login", (req, res) => {
-    res.send("Login")
-})
-router.get("/logout", (req, res) => {
-    res.send("Logout")
-})
+router.get("/signup", signup);
+
+router.get("/login", login);
+
+router.get("/logout", logout);
 
 export default router;
