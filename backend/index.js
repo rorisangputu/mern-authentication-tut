6aaf05dbd04f2911a75facc2b872aba455a62185
Rorisang Putu <rorisangputu@gmail.com>
2025-02-16 14:57:46 +0200

    changing port

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,12 @@ import authRoutes from './routes/auth.route.js'
 const databaseConn = connectDb();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send({message: "Helloooo"})
-})
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     databaseConn
-    console.log("Listening on Port 3000")
+    console.log("Listening on Port 5000")
 })
