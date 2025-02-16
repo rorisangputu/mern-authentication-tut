960bf218784b0d79d283ee5eb64e491b325b931b
Rorisang Putu <rorisangputu@gmail.com>
2025-02-16 15:04:19 +0200

    desctructing req body

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose"
 import bcryptjs from 'bcryptjs'
 
 export const signup = async (req, res) => {
-    
+    const { email, password, name } = req.body;
+
+    try {
+        
+    } catch {
+        
+    }
 }
 export const login = async (req, res) => {
 
