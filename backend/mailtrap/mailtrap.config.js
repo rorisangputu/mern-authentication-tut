b967cf6565496f4e68637b196cbdf676086165f7
Rorisang Putu <rorisangputu@gmail.com>
2025-02-20 23:05:44 +0200

    removing boilerplate

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -5,26 +5,13 @@ dotenv.config();
 const TOKEN = process.env.MAILTRAP_TOKEN
 const ENDPOINT = process.env.MAILTRAP_ENDPOINT
 
-const client = new MailtrapClient({endpoint: ENDPOINT,
+export const client = new MailtrapClient({endpoint: ENDPOINT,
   token: TOKEN,
 });
 
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.com",
   name: "Mailtrap Test",
 };
-const recipients = [
-  {
-    email: "rorisangputu@gmail.com",
-  }
-];
 
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
+
