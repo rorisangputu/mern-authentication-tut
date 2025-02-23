eaf6f1673c11e3d6d369315cbc590dbff44d0a76
Rorisang Putu <rorisangputu@gmail.com>
2025-02-23 15:26:24 +0200

    creating new function that'll handle sending Welcome Email

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -18,3 +18,7 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error(`Error sending verification email: ${error}`)
     }
 }
+
+export const sendWelcomeEmail = async (email, name) => {
+    
+}
