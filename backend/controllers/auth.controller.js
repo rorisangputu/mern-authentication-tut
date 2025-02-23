e1be3e189bdd2696d31cc980a7c3d556c1cb0f51
Rorisang Putu <rorisangputu@gmail.com>
2025-02-23 15:52:40 +0200

    importing sendWelcome Email func

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcryptjs from 'bcryptjs'
 import { User } from '../models/user.model.js';
 import { generateVerificationCode } from "../utils/generateVerificationCode.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../mailtrap/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
